a8552dc33ee40406e026585905d2938a1cc4643e
Yafiet Isaac <yafietisaac@msn.com>
2021-04-06 21:12:58 -0700

    :code

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
-router.use('/api', require('./exerciseRoutes.js'))
-
+router.use('/api', require('./workoutRoutes.js'))
+router.use('/api', require('./viewRoutes.js'))
 
 module.exports = router
